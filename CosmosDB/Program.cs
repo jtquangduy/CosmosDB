50c86ca12f2499e05202ed381a9b52593b707489
Quang Duy <justinquangduy@gmail.com>
2023-05-23 22:51:10 +0700

    Adding an item

--- CosmosDB/Program.cs
+++ CosmosDB/Program.cs
@@ -1,28 +1,55 @@
-﻿using Microsoft.Azure.Cosmos;
+﻿using CosmosDB;
+using Microsoft.Azure.Cosmos;
 
 string cosmosEndpointUri = "https://qdapp.documents.azure.com:443/";
 string cosmosDBKey = "UirRsYNaZmZ0zDaZhJ557omccnaU1pZRHrx0s3hPAsRLyZkXb5BFfwP4b22jWQWR3FTCkzEyuE7dACDbLJB73g==";
+string databaseName = "qddb";
+string containerName = "Orders";
 
 //await CreateDatabase("qddb");
-await CreateContainer("qddb","Orders","/category");
+//await CreateContainer("qddb","Orders","/category");
 
+//async Task CreateDatabase(string dbName)
+//{
+//    CosmosClient cosmosClient = new CosmosClient(cosmosEndpointUri, cosmosDBKey);
 
-async Task CreateDatabase(string dbName)
+//    await cosmosClient.CreateDatabaseAsync(dbName);
+//    Console.WriteLine("Database created");
+//}
+
+//async Task CreateContainer(string databaseName,string containerName,string partitionKey)
+//{
+//    CosmosClient cosmosClient = new CosmosClient(cosmosEndpointUri, cosmosDBKey);
+
+//    Database database=cosmosClient.GetDatabase(databaseName);
+
+//    await database.CreateContainerAsync(containerName, partitionKey);
+//    Console.WriteLine("Container created");
+
+//}
+
+await AddItem("O1", "Laptop", 100);
+await AddItem("O2", "Mobiles", 200);
+await AddItem("O3", "Desktop", 75);
+await AddItem("04", "Laptop", 25);
+
+async Task AddItem(string orderId, string category, int quantity)
 {
     CosmosClient cosmosClient = new CosmosClient(cosmosEndpointUri, cosmosDBKey);
 
-    await cosmosClient.CreateDatabaseAsync(dbName);
-    Console.WriteLine("Database created");
-}
+    Database database = cosmosClient.GetDatabase(databaseName);
+    Container container = database.GetContainer(containerName);
 
+    Order order = new Order()
+    {
+        id = Guid.NewGuid().ToString(),
+        orderId = orderId,
+        category = category,
+        quantity = quantity
+    };
 
-async Task CreateContainer(string databaseName,string containerName,string partitionKey)
-{
-    CosmosClient cosmosClient = new CosmosClient(cosmosEndpointUri, cosmosDBKey);
+    ItemResponse<Order> response = await container.CreateItemAsync<Order>(order, new PartitionKey(category));
 
-    Database database=cosmosClient.GetDatabase(databaseName);
-
-    await database.CreateContainerAsync(containerName, partitionKey);
-    Console.WriteLine("Container created");
-
+    Console.WriteLine("Added item with Order Id {0}", order.orderId);
+    Console.WriteLine("Request Units {0}", response.RequestCharge);
 }
