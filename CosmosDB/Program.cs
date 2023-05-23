d5580f9561d7d26f297adf85f5c60cdbfbf158ec
Quang Duy <justinquangduy@gmail.com>
2023-05-23 22:11:49 +0700

    Add project files.

---
+++ CosmosDB/Program.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Azure.Cosmos;
+
+string cosmosEndpointUri = "https://qdapp.documents.azure.com:443/";
+string cosmosDBKey = "UirRsYNaZmZ0zDaZhJ557omccnaU1pZRHrx0s3hPAsRLyZkXb5BFfwP4b22jWQWR3FTCkzEyuE7dACDbLJB73g==";
+
+//await CreateDatabase("qddb");
+await CreateContainer("qddb","Orders","/category");
+
+
+async Task CreateDatabase(string dbName)
+{
+    CosmosClient cosmosClient = new CosmosClient(cosmosEndpointUri, cosmosDBKey);
+
+    await cosmosClient.CreateDatabaseAsync(dbName);
+    Console.WriteLine("Database created");
+}
+
+
+async Task CreateContainer(string databaseName,string containerName,string partitionKey)
+{
+    CosmosClient cosmosClient = new CosmosClient(cosmosEndpointUri, cosmosDBKey);
+
+    Database database=cosmosClient.GetDatabase(databaseName);
+
+    await database.CreateContainerAsync(containerName, partitionKey);
+    Console.WriteLine("Container created");
+
+}
